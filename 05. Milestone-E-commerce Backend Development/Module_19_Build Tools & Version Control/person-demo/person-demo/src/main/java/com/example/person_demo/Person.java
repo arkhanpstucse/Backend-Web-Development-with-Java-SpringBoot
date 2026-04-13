c20148df294458c896515e32d2228f61954abeee
arkhanpstucse <abdur.rahman.khan.01502@gmail.com>
2026-04-13 21:48:31 +0600

    Add Person class with basic fields and methods

---
+++ 05. Milestone-E-commerce Backend Development/Module_19_Build Tools & Version Control/person-demo/person-demo/src/main/java/com/example/person_demo/Person.java
@@ -0,0 +1,48 @@
+package com.example.persondemo;
+
+public class Person {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String email;
+
+    // Default constructor
+    public Person() {}
+
+    // Parameterized constructor
+    public Person(Long id, String firstName, String lastName, int age, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.email = email;
+    }
+
+    // Getters and Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
+    public int getAge() { return age; }
+    public void setAge(int age) { this.age = age; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
